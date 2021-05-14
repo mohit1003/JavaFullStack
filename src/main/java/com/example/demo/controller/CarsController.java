6544ec87eeff06cd82a023c42edaac120839e237
Mohit <mpkulkarni02@gmail.com>
2021-05-14 18:48:33 +0530

    Integrated backend to front end

--- src/main/java/com/example/demo/controller/CarsController.java
+++ src/main/java/com/example/demo/controller/CarsController.java
@@ -1,12 +1,21 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.system.ApplicationTemp;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 //import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +23,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.model.CarsModel;
 import com.example.demo.service.CarService;
+import com.sun.istack.NotNull;
 
 @Controller
 public class CarsController {
@@ -21,8 +31,10 @@ public class CarsController {
 	@Autowired
 	private CarService service;
 	
+	private List<CarsModel> carResult;
+	
 	@GetMapping(value = "/")
-	public String getHomePage() {
+	public String getHomePage(Model m) {
 		return "index.html";
 	}
 	
@@ -32,14 +44,47 @@ public class CarsController {
 	}
 	
 	@GetMapping(value = "/searchByBrand")
-	public String getSearchByBrandPage() {
-		List<String> brandList = service.getCarManufacturerList();
+	public String getSearchByBrandPage(Model carManufacturers) {
+//		carManufacturers.addAttribute("brand", new String());
+		carManufacturers.addAttribute("carManufacturers", new HashSet<>(service.getCarManufacturerList()));
 		return "userSelectedBrand.html";
 	}
 	
 	@GetMapping(value = "/carsSearchResult")
-	public String getCarSearchResult() {
-		List<CarsModel> carsModels = service.findByBrandName("Tata");
+	public String getCarSearchResult(@Valid @NotEmpty(message = "Brand cannot be empty") String brand, Model selectedBrandCars) {
+		if(brand == null || brand.equals("0")) {
+			selectedBrandCars.addAttribute("carManufacturers", new HashSet<>(service.getCarManufacturerList()));
+			selectedBrandCars.addAttribute("error", "Please select an option");
+			return "userSelectedBrand.html";
+		}
+		List<CarsModel> carsModels = service.findByBrandName(brand);
+		carResult = new ArrayList<>();
+		carResult = carsModels;
+		selectedBrandCars.addAttribute("selectedBrandCars", carsModels);
+		return "carsSearchResult.html";
+	}
+	
+	@GetMapping(value = "/carsSearchResultPrice")
+	public String getCarSearchResultPrice(@Valid String price, Model selectedBrandCars) {
+		carResult = new ArrayList<>();
+		if(selectedBrandCars.containsAttribute("error")) {
+			selectedBrandCars = null;
+		}
+		if(price == null) {
+			selectedBrandCars.addAttribute("error", "Please select an option");
+			return "userSelectedPrice.html";
+		}
+		if(price.equalsIgnoreCase("gt5")) {
+			carResult = service.findByCarPriceMoreThan5L();
+			selectedBrandCars.addAttribute("selectedBrandCars", carResult);
+		}
+		else {
+			carResult = service.findByCarPriceLessThan5L();
+			selectedBrandCars.addAttribute("selectedBrandCars", carResult);
+		}
+		
+
+		
 		return "carsSearchResult.html";
 	}
 	
@@ -48,9 +93,21 @@ public class CarsController {
 		return "userSelectedBrand.html";
 	}
 	
-	@GetMapping(value = "/userSelectedPrice")
-	public String getuserSelectedPricePage() {
+	@GetMapping(value = "/searchByPrice")
+	public String getuserSelectedPricePage( Model m) {
+		m.addAttribute("price", "");
 		return "userSelectedPrice.html";
+	}
+	
+	@GetMapping(value = "/bookingConfirm")
+	public String bookingDone(@Valid String id, Model m) {
+		System.out.println("+++++++++++ "+id);
+		if(id == null) {
+			m.addAttribute("error", "Please select an Option");
+			m.addAttribute("selectedBrandCars", carResult);
+			return "carsSearchResult.html";
+		}
+		return "bookingconfirm.html";
 	}
 	
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-insert into CARS_MODEL values('Hyundai', 'i10', '2019', '10000', '300000','Petrol');
-insert into CARS_MODEL values('Hyundai', 'i20', '2018', '20000', '20000','Diesel');
-insert into CARS_MODEL values('Hyundai', 'Verna', '2015', '15000', '250000','Petrol');
-insert into CARS_MODEL values('Tata', 'Bolt', '2019', '15000', '250000','Petrol');
-insert into CARS_MODEL values('Maruthi Suzuki', 'Ciaz', '2019', '5000', '600000','Petrol');
-insert into CARS_MODEL values('Honda', 'City', '2019', '15000', '400000','Diesel');
-insert into CARS_MODEL values('Maruthi Suzuki', 'Ertiga', '2018', '10000', '550000','Petrol');
-insert into CARS_MODEL values('Tata', 'Nexon', '2019', '5000', '600000','Petrol');
+insert into CARS_MODEL values(1,'Hyundai', 'i10', '2019', '10000', '300000','Petrol');
+insert into CARS_MODEL values(2,'Hyundai', 'i20', '2018', '20000', '20000','Diesel');
+insert into CARS_MODEL values(3,'Hyundai', 'Verna', '2015', '15000', '250000','Petrol');
+insert into CARS_MODEL values(4,'Tata', 'Bolt', '2019', '15000', '250000','Petrol');
+insert into CARS_MODEL values(5,'Maruthi Suzuki', 'Ciaz', '2019', '5000', '600000','Petrol');
+insert into CARS_MODEL values(6,'Honda', 'City', '2019', '15000', '400000','Diesel');
+insert into CARS_MODEL values(7,'Maruthi Suzuki', 'Ertiga', '2018', '10000', '550000','Petrol');
+insert into CARS_MODEL values(8,'Tata', 'Nexon', '2019', '5000', '600000','Petrol');
 
 
 
